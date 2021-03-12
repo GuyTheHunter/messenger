e4db08ab0b1d9d7af4ef4fb1311073f79bf1cbdc
GuyTheHunter <guy19419@gmail.com>
2021-03-12 09:06:10 +0200

    Bug fixed, resets hash after usage

--- server/crypto/__init__.py
+++ server/crypto/__init__.py
@@ -1,4 +1,4 @@
 import hashlib
 from .user_crypto import UserCrypto as userCrypto
 
-UserCrypto = userCrypto(hashlib.sha256())
+UserCrypto = userCrypto(hashlib.sha256)
--- server/crypto/user_crypto.py
+++ server/crypto/user_crypto.py
@@ -2,8 +2,14 @@ from .base_crypto import BaseCrypto
 
 
 class UserCrypto(BaseCrypto):
-    def __init__(self, hasher):
-        self._hasher = hasher
+    def __init__(self, hasher_cls):
+        self._hasher_cls = hasher_cls
+
+    def __init_hahser(self):
+        """
+        Initials the hasher object
+        """
+        self._hasher = self._hasher_cls()
 
     def encode(self, username: str, password: str) -> str:
         """
@@ -16,9 +22,10 @@ class UserCrypto(BaseCrypto):
         Returns:
             str: Encoded user's password
         """
+        self.__init_hahser()
+
         salted_password = self._salt(username, password)
         self._hasher.update(salted_password.encode('utf8'))
-
         return self._hasher.hexdigest()
 
     @staticmethod
