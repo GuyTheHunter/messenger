95dd8f96e7476f8cb58497d80c2eb8637db03ec3
GuyTheHunter <guy19419@gmail.com>
2021-03-26 20:28:02 +0300

    Added error view and improved onclick handling

--- android/app/src/main/java/com/guythehunter/messanger/LoginActivity.java
+++ android/app/src/main/java/com/guythehunter/messanger/LoginActivity.java
@@ -1,16 +1,18 @@
 package com.guythehunter.messanger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText username_input, password_input;
     private Button login_btn, register_btn;
+    private TextView error_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +26,16 @@ public class LoginActivity extends AppCompatActivity {
      * Sets the components of the activity
      */
     private void setComponents() {
+        error_text = (TextView) this.findViewById(R.id.error_text);
+
         username_input = (EditText) this.findViewById(R.id.username_input);
         password_input = (EditText) this.findViewById(R.id.password_input);
 
         login_btn = (Button) this.findViewById(R.id.login_button);
         register_btn = (Button) this.findViewById(R.id.register_button);
 
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login(username_input.getText().toString(), password_input.getText().toString());
-            }
-        });
-        register_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register(username_input.getText().toString(), password_input.getText().toString());
-            }
-        });
+        login_btn.setOnClickListener(this);
+        register_btn.setOnClickListener(this);
     }
 
     /**
@@ -62,5 +56,34 @@ public class LoginActivity extends AppCompatActivity {
      */
     private void register(String username, String password) {
 
+    }
+
+    /**
+     * Clears the error message
+     */
+    private void clearError() {
+        this.error_text.setText(R.string.empty_string);
+    }
+
+    /**
+     * Handles on click event
+     *
+     * @param v View the was clicked
+     */
+    @Override
+    public void onClick(View v) {
+        clearError();
+        String username = this.username_input.getText().toString(),
+                password = this.password_input.getText().toString();
+
+        switch (v.getId()) {
+            case R.id.login_button:
+                this.login(username, password);
+                break;
+
+            case R.id.register_button:
+                this.register(username, password);
+                break;
+        }
     }
 }
